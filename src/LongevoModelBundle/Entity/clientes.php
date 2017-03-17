ca4ab14aa6f037ca25fb526f6d414150e92fae00
Bruno Brabo <bruno.brabo@predicta.net>
2017-03-17 02:49:10 -0300

    adicionar Entity Clientes

---
+++ src/LongevoModelBundle/Entity/clientes.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace LongevoModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * clientes
+ *
+ * @ORM\Table(name="clientes")
+ * @ORM\Entity(repositoryClass="LongevoModelBundle\Repository\clientesRepository")
+ */
+class clientes
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nome", type="string", length=50)
+     */
+    private $nome;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=50)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nome
+     *
+     * @param string $nome
+     * @return clientes
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get nome
+     *
+     * @return string 
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return clientes
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
